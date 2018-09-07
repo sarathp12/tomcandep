3e2ef260c549c891250d9c443c95b3a73fd9c7d5
sarathp12 <psarathyp18@gmail.com>
2018-09-07 13:01:41 -0400

    corrections made to delete previous artefacts in copy script

--- for-copy.sh
+++ for-copy.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 cd /var/lib/jenkins/workspace/simple-webapp-tomcat/target
-ssh jenkins@192.168.144.143 rm -rf *.war
+ssh jenkins@192.168.144.143 rm -rf /home/jenkins/*.war
 sudo scp *.war jenkins@192.168.144.143:/home/jenkins
 
 
