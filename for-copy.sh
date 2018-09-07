cf8e46ef7f264b9275a9893bada62ea865a847eb
sarathp12 <psarathyp18@gmail.com>
2018-09-07 11:45:46 -0400

    workflow in script is corrected

--- for-copy.sh
+++ for-copy.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
 
-cd /var/lib/jenkins/workspace/simple-webapp-tomcat
-
-ssh jenkins@192.168.144.143
-
-rm -rf webproject.war
-
-exit
-
+cd /var/lib/jenkins/workspace/simple-webapp-tomcat/target
+ssh jenkins@192.168.144.143 rm -rf *.war
 sudo scp *.war jenkins@192.168.144.143:/home/jenkins
 
 
