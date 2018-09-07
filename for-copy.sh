4425b4ae20721350294267eb326081144b586e22
sarathp12 <psarathyp18@gmail.com>
2018-09-07 13:08:31 -0400

    ssh step deleted as there is no use

--- for-copy.sh
+++ for-copy.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 cd /var/lib/jenkins/workspace/simple-webapp-tomcat/target
-ssh jenkins@192.168.144.143 rm -rf /home/jenkins/*.war
 sudo scp *.war jenkins@192.168.144.143:/home/jenkins
 
 
